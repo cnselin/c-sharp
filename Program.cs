6e71d1bc3e5c91978a4fa49cafc06c5014f88f15
cnselin <115400670+cnselin@users.noreply.github.com>
2023-07-05 15:53:05 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,27 +1,31 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
-namespace GenericsIntro
+namespace Koleksiyonlar
 {
     class Program
     {
         static void Main(string[] args)
         {
-            MyList<string> isimler = new MyList<string>();
-            isimler.Add("Engin");
+            //string[] isimler = new string[] { "Engin", "Murat", "Kerem", "Halil" };
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+            //Console.WriteLine(isimler[3]);
+            //isimler = new string[5];
+            //isimler[4] = "İlker";
+            //Console.WriteLine(isimler[4]);
+            //Console.WriteLine(isimler[0]);
 
-            Console.WriteLine(isimler.Length);
+            List<string> isimler2 = new List<string> { "Engin", "Murat", "Kerem", "Halil" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
 
-            isimler.Add("Kerem");
 
-            Console.WriteLine(isimler.Length);
-
-            foreach (var isim in isimler.Items)
-            {
-                Console.WriteLine(isim);
-            }
-
-            Console.WriteLine("Hello World!");
 
         }
     }

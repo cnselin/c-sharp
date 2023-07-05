5fb413b51a82ee84f4974cc57b5c13e6c09d6ca4
cnselin <115400670+cnselin@users.noreply.github.com>
2023-07-05 15:47:32 +0300

    c# 3.gün

--- Program.cs
+++ Program.cs
@@ -1,32 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace OOP1
+namespace GenericsIntro
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Product product1 = new Product(); //101
-            product1.Id = 1;
-            product1.CategoryId = 2;
-            product1.ProductName = "Masa";
-            product1.UnitPrice = 500;
-            product1.UnitsInStock = 3;
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Engin");
 
-            Product product2 = new Product {Id = 2, CategoryId=5, 
-                UnitsInStock = 5, ProductName="Kalem", UnitPrice= 35};
+            Console.WriteLine(isimler.Length);
 
-            //PascalCase   //camelCase
-            //case sensitive
-            ProductManager productManager = new ProductManager(); 
-            productManager.Add(product1);
-            Console.WriteLine(product1.ProductName);
+            isimler.Add("Kerem");
 
+            Console.WriteLine(isimler.Length);
 
+            foreach (var isim in isimler.Items)
+            {
+                Console.WriteLine(isim);
+            }
 
-            //int,double,bool... değer tip
-            //diziler,class, abstract class, interface ... referans tip
-            //ref out
+            Console.WriteLine("Hello World!");
+
         }
     }
 }
